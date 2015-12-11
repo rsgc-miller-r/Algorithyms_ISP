949cc14d40c2728fceb177fc2d9bcb1cfd44889f
rsgc-miller-r <rmiller@rsgc.on.ca>
2015-12-11 09:54:27 -0500

    Added animation things
    yeah

---
+++ Animation/Sketch.swift
@@ -0,0 +1,55 @@
+//
+//  Sketch.swift
+//  Animation
+//
+//  Created by Russell Gordon on 2015-12-05.
+//  Copyright © 2015 Royal St. George's College. All rights reserved.
+//
+
+import Foundation
+
+class Sketch {
+
+    // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
+    //       Therefore, the line immediately below must always be present.
+    let canvas : Canvas
+    
+    // Declare any properties you need for your sketch below this comment, but before init()
+    var x = 0
+    var s = 1
+
+    // This runs once, equivalent to setup() in Processing
+    init() {
+        
+        // Create canvas object – specify size
+        canvas = Canvas(width: 700, height: 100)
+        
+        // The frame rate can be adjusted; the default is 60 fps
+        canvas.framesPerSecond = 60
+        
+    }
+    
+    // Runs repeatedly, equivalent to draw() in Processing
+    func draw() {
+                
+        // Horizontal position of circle
+        x = x + s
+        
+        // Bounce when hitting wall
+        if (x > canvas.width || x < 0) {
+            s *= -1
+        }
+        
+        // Clear the background
+        canvas.drawShapesWithBorders = false
+        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 100)
+        canvas.drawRectangle(bottomRightX: 0, bottomRightY: 0, width: canvas.width, height: canvas.height)
+        
+        // Draw a circle that moves across the screen
+        canvas.drawShapesWithBorders = false
+        canvas.fillColor = Color(hue: Float(canvas.frameCount), saturation: 80, brightness: 90, alpha: 100)
+        canvas.drawEllipse(centreX: x, centreY: canvas.height / 2, width: 25, height: 25)
+        
+    }
+    
+}
